bca0c8438faee1ddb406d120d6d60d76d7cc47b1
root <root@develope>
2017-03-02 14:46:38 -0500

    Se añaden los archivos de estilo y de javascript para la configuracion visual del programa.

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-02T10:51:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-02T13:29:44")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
