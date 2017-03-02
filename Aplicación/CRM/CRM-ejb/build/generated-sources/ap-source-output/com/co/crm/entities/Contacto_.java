2578b42770a10876879e6c40464399f36462cbc6
root <root@develope>
2017-03-02 10:58:29 -0500

    Se añadieron los estilos de la pagina de login, se modifica el formulario

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Contacto_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Contacto_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-01T20:23:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-02T10:51:49")
 @StaticMetamodel(Contacto.class)
 public class Contacto_ { 
 
