53bc7d85e8df1709a30905b2d6051bf5a0cb9482
root <root@develope>
2017-03-04 00:10:55 -0500

    se corrige un error de css que no permitia adaptar el contenido a pantalla

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-02T13:29:44")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-02T21:22:34")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
