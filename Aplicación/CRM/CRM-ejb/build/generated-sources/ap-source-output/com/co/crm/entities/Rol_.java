a400c902b2635d10cf06cbe76f0fc524ce5a61b9
samuel <samuel@samuel>
2017-03-08 16:28:01 -0500

    Se configura el estilo de los formularios

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Rol_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Rol_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-08T07:47:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-08T09:04:41")
 @StaticMetamodel(Rol.class)
 public class Rol_ { 
 
