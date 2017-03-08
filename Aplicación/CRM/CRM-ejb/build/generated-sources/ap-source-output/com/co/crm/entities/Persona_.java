3876273beb3a7d3a50040c9563cf961494c1b5cf
samuel <samuel@samuel>
2017-03-08 18:07:37 -0500

    se agrega logo blanco y se posiciona un poco mejor en la pagina de entrada, se quita una seccion para limpiar el contenido de la pagina principal

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Persona_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Persona_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-08T09:04:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-08T16:36:22")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
