08b462c9a8c083aa52e2414f03c3d995edff3981
samuel <darkdemon.9006@gmail.com>
2017-03-11 04:30:35 -0500

    Se fusionan los proyectos para remediar un incombeniente en el que los proyectos se encontraban separados

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Persona_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Persona_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-10T02:30:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-11T04:29:03")
 @StaticMetamodel(Persona.class)
 public class Persona_ { 
 
--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-10T02:30:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-11T04:29:03")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
