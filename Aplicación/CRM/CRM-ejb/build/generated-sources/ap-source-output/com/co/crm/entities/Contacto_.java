d9fc5dfa89272de7644bd5907f425eaa692bdea7
samuel villanueva pineda <darkdemon.9006@gmail.com>
2017-03-25 08:13:16 +0100

    Se actualiza la interfaz de usuario, se agregan iconos en las tablas

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Contacto_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Contacto_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-25T00:48:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-03-25T07:46:34")
 @StaticMetamodel(Contacto.class)
 public class Contacto_ { 
 
