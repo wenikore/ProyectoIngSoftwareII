263d91ab9336d6b1bad6f439b23f8e64642554a1
Jhon Jairo chaparro <Jhon Jairo chaparro>
2017-04-19 21:09:41 -0500

    configuracion de vistas en front-end

--- Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
+++ Aplicación/CRM/CRM-ejb/build/generated-sources/ap-source-output/com/co/crm/entities/Usuario_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-19T21:24:49")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-19T20:55:30")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
